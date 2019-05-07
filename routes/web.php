0726c3727235d2204e92572946d937d3e8d1370d
Kadiri Kazeem <kazeem.kadiri@gmail.com>
2019-05-07 11:07:47 +0100

    Added new string length for schema in AppSeviceProvider

--- routes/web.php
+++ routes/web.php
@@ -15,7 +15,15 @@ Route::get('/', function () {
     return view('welcome');
 });
 
+Auth::routes();
+
+Route::get('login', 'Auth\LoginController@showLoginForm');
+
 Route::get('/product-catalog', 'AdminController@product_catalog');
 
 Route::get('/single-product', 'AdminController@single_product');
 
+
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
